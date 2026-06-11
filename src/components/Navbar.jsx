de80b7a1526ebd0d96fc8a4b9a299cb6ab3c7803
amarinder singh <amarindersingh@amarinders-MacBook-Pro.local>
2026-06-11 16:47:22 +0530

    new logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Menu, X } from "lucide-react";
-import logo from "../assets/logo.png"
+import logo from "../assets/realtorlogo.png"
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
